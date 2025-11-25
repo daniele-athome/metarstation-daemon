20ea2cdc99b92926cfc42497617ec8ed244bac19
Daniele Ricci <daniele@casaricci.it>
2025-11-25 19:11:49 +0100

    Big Bang

---
+++ weather-daemon.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env -S uv run --script
+# /// script
+# requires-python = ">=3.10"
+# dependencies = ["bleak", "bthome-ble"]
+# ///
+
+import sys
+
+
+class WeatherDaemon:
+    def __init__(self, args):
+        pass
+
+    def run(self):
+        pass
+
+
+if __name__ == '__main__':
+    WeatherDaemon(sys.argv).run()
