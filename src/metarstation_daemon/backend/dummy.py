9d9a40fe9784cdcaa00b8f8d352d4b5830bd0e95
Daniele Ricci <daniele@casaricci.it>
2026-09-21 23:18:53 +0200

    Dummy sensor data

--- src/metarstation_daemon/backend/dummy.py
+++ src/metarstation_daemon/backend/dummy.py
@@ -32,9 +32,20 @@ class DummySensorBackend(SensorBackend):
     async def _collect_data_start(self):
         while self._running:
             try:
-                # TODO fabricate data
                 self.queue.push(SensorData(
                     timestamp=datetime.datetime.now(),
+                    battery=100,
+                    temperature=20.2,
+                    humidity=64,
+                    dew_point=13.16,
+                    pressure=997.6,
+                    illumination=0,
+                    wind_speed=0,
+                    gust_speed=0,
+                    wind_direction=146,
+                    uv_index=0,
+                    raining=None,
+                    precipitation=331.4,
                 ))
 
             except asyncio.CancelledError:
